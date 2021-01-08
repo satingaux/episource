4b4e295b2acfa71dcff1e02ca1b18e048959286c
Sachin Sharma <satingaux@gmail.com>
2021-01-08 16:52:30 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,14 +4,19 @@ import sys
  
 
  
-# total arguments
-print(sys.argv)
-lastVersion = sys.argv[1]
-currentVersion = sys.argv[2]
-n = len(sys.argv)
-print("Total arguments passed:", n)
+def main():
+ # total arguments
+ print(sys.argv)
+ lastVersion = sys.argv[1]
+ currentVersion = sys.argv[2]
+ n = len(sys.argv)
+ print("Total arguments passed:", n)
 
 
-isCurrentVersionCorrect = lastVersion <= currentVersion
+ isCurrentVersionCorrect = lastVersion <= currentVersion
 
-print(isCurrentVersionCorrect)
+ print(isCurrentVersionCorrect)
+ return isCurrentVersionCorrect
+
+
+main()
