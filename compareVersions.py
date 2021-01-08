037d4ba4a94cf37194a2e84f5e1abb232cefe492
Sachin Sharma <satingaux@gmail.com>
2021-01-08 18:15:18 +0530

    Update compareVersions.py

--- compareVersions.py
+++ compareVersions.py
@@ -3,22 +3,22 @@ from collections import defaultdict
 from github import Github
 
 
- 
-n = len(sys.argv)
+print(sys.argv)
+# n = len(sys.argv)
 
-if n == 3:
- lastVersion = sys.argv[1]
- currentVersion = sys.argv[2]
- print(lastVersion <= currentVersion)
-else:
- print('INVALID ARGS')
+# if n == 3:
+#  lastVersion = sys.argv[1]
+#  currentVersion = sys.argv[2]
+#  print(lastVersion <= currentVersion)
+# else:
+#  print('INVALID ARGS')
  
  
-access_token = sys.argv[3]
-print(access_token)
-g = Github(access_token)
+# access_token = sys.argv[3]
+# print(access_token)
+# g = Github(access_token)
 
-print(g)
+# print(g)
 # for repo in g.get_user().get_repos():
 #     print(repo.name)
 
