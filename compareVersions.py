73f73eadce02520ab4b4cd280d89b8626bc5a095
Sachin Sharma <satingaux@gmail.com>
2021-01-08 17:50:25 +0530

    Update compareVersions.py

--- compareVersions.py
+++ compareVersions.py
@@ -1,6 +1,8 @@
 import sys
 from collections import defaultdict
 from github import Github
+
+
  
 n = len(sys.argv)
 
@@ -10,3 +12,14 @@ if n == 3:
  print(lastVersion <= currentVersion)
 else:
  print('INVALID ARGS')
+ 
+ 
+access_token = sys.arv[3]
+
+GH = Github(access_token)
+
+for repo in g.get_user().get_repos():
+    print(repo.name)
+
+
+
