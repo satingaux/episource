d1f7540366713d529864f412d91cc5bc11d18c3a
Sachin Sharma <satingaux@gmail.com>
2021-01-08 17:32:10 +0530

    Update compareVersions.py

--- compareVersions.py
+++ compareVersions.py
@@ -5,4 +5,6 @@ n = len(sys.argv)
 if n == 3:
  lastVersion = sys.argv[1]
  currentVersion = sys.argv[2]
- return lastVersion <= currentVersion
+ print(lastVersion <= currentVersion)
+else:
+ print('INVALID ARGS')
