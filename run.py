515212ec51f92c7934254df63e124dc0f523d6b9
Sachin Sharma <satingaux@gmail.com>
2021-01-08 16:46:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,16 @@ print('sachin')
 
 import sys
  
+
+ 
 # total arguments
 print(sys.argv)
+lastVersion = sys.argv[1]
+currentVersion = sys.argv[2]
 n = len(sys.argv)
 print("Total arguments passed:", n)
+
+
+isCurrentVersionCorrect = lastVersion <= currentVersion
+
+print('isCurrentVersionCorrect: ' isCurrentVersionCorrect)
