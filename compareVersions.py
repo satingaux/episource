bf3d1bd4bced78d561b306c9beeef93b6784762b
Sachin Sharma <satingaux@gmail.com>
2021-01-11 17:00:45 +0530

    Update compareVersions.py

--- compareVersions.py
+++ compareVersions.py
@@ -1,5 +1,5 @@
-import sys
-from collections import defaultdict
+# import sys
+# from collections import defaultdict
 # from github import Github
 
 # argumnts
@@ -7,14 +7,14 @@ from collections import defaultdict
 # 2. Current Version
 
 # print(sys.argv)
-n = len(sys.argv)
+# n = len(sys.argv)
 
-if n == 3:
- lastVersion = sys.argv[1]
- currentVersion = sys.argv[2]
- print(lastVersion >= currentVersion)
-else:
- print('INVALID ARGUMENTS')
+# if n == 3:
+#  lastVersion = sys.argv[1]
+#  currentVersion = sys.argv[2]
+#  print(lastVersion >= currentVersion)
+# else:
+#  print('INVALID ARGUMENTS')
  
 
 # g = Github(sys.argv[3])
@@ -24,6 +24,16 @@ else:
 #     print(repo.name)
   
 
+import os
+import github
+import sys
 
+print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
+def get_inputs(input_name):
+  return os.getenv(input_name.upper())
+  
+  
+print(get_inputs('GITHUB_TOKEN'))
 
 
